c9c0a4dca9c74dd3cd0dd271e11a44b1d6513ed2
NicolasR1 <nretamalb1@gmail.com>
2022-04-02 15:40:08 -0300

    Suavizado de fuente y corrección de titulos LOREM v1.2

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,23 +1,16 @@
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
+    return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
-$(function(){
+$(function () {
 
-    $('#enviar').click(function(){
-      alert('Su mensaje ha sido enviado con exito.');
+    $('#enviar').click(function () {
+        alert('Su mensaje ha sido enviado con exito.');
     });
-  
-    
-    $("h2").dblclick(function(){
-      $(this).css({
-        color: "red",
-        borderColor: "red"
-      });
+
+
+    $(".card-title").click(function () {
+        $(".card-text").toggle()
     })
-    
-    $(".card-title").click(function(){
-      $(".card-text").toggle()
-    })
-  });
+});
